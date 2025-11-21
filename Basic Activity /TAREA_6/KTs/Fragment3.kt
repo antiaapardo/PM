011bf31e0d9326084b4ce1a041bd7d764f8cb293
antiaapardo <pardojimenezantia@gmail.com>
2025-11-21 10:03:46 +0100

    Create Fragment3.kt

---
+++ Basic Activity /TAREA_6/KTs/Fragment3.kt
@@ -0,0 +1,27 @@
+package com.example.dashboard
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.dashboard.databinding.Fragment3Binding
+
+class Fragment3 : Fragment() {
+
+    private var _binding: Fragment3Binding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = Fragment3Binding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
